acc08cc475fdcbdff08544932b2125ab31f808eb
pichu185 <pichu_otegui@hotmail.com>
2021-03-16 22:15:42 +0000

    updated header

--- holberton.h
+++ holberton.h
@@ -25,7 +25,4 @@ int print_char(va_list lista);
 int _putchar(char c);
 int print_int(va_list lista);
 int print_porcentaje(va_list lista);
-int print_unsigned(va_list lista);
-int print_octal(va_list lista);
-int print_binary(va_list lista);
 #endif
